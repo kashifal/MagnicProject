f895d6d2a596cd0d92940f9600c8380528e53b45
kashifal <captainkashi4@gmail.com>
2022-02-04 19:37:00 +0500

    carousel added

--- app.js
+++ app.js
@@ -30,4 +30,37 @@ for (var i = 0; i < label.length; i++) {
   });
 }
 
+ $(document).ready(function () {
+                    $('.carousel').slick({
+                        slidesToShow: 4,
+                        centerMode: true,
+                        autoplay:true,
+                        infinite:true,
+                        responsive: [
+                            {
+                                breakpoint: 1024,
+                                settings: {
+                                    slidesToShow: 3,
+                                    slidesToScroll: 3,
+                                    infinite: true, 
+                                }
+                            },
+                            {
+                                breakpoint: 600,
+                                settings: {
+                                    slidesToShow: 1,
+                                    slidesToScroll: 2
+                                }
+                            },
+                            {
+                                breakpoint: 480,
+                                settings: {
+                                    slidesToShow: 1,
+                                    slidesToScroll: 1
+                                }
+                            }
+                        ]
+                    });
+                });
+
 
