97f048500e2c9bd6ef7d873c7a93a8e36a9faa04
Mohammed Kashif <65946187+kashifal@users.noreply.github.com>
2022-02-08 16:36:13 +0500

    cardChanges

--- app.js
+++ app.js
@@ -23,56 +23,56 @@ var radio = document.getElementById('radio')
 var label = radio.getElementsByClassName("not-active");
 
 for (var i = 0; i < label.length; i++) {
-  label[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active"; 
+  label[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
   });
 }
 
- $(document).ready(function () {
-                    $('.carousel').slick({
-                        slidesToShow: 4,
-                        centerMode: true,
-                        autoplay:true,
-                        infinite:true,
-                        responsive: [
-                            {
-                                breakpoint: 1024,
-                                settings: {
-                                    slidesToShow: 2,
-                                    slidesToScroll: 3,
-                                    infinite: true, 
-                                }
-                            },
-                            {
-                                breakpoint: 600,
-                                settings: {
-                                    slidesToShow: 1,
-                                    slidesToScroll: 2
-                                }
-                            },
-                            {
-                                breakpoint: 480,
-                                settings: {
-                                    slidesToShow: 1,
-                                    slidesToScroll: 1
-                                }
-                            }
-                        ]
-                    });
-                });
+$(document).ready(function () {
+  $('.carousel').slick({
+    slidesToShow: 4,
+    centerMode: true,
+    autoplay: true,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 3,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
 
 
 
 
-                document.addEventListener("DOMContentLoaded", function() {
-  var lazyloadImages;    
+document.addEventListener("DOMContentLoaded", function () {
+  var lazyloadImages;
 
   if ("IntersectionObserver" in window) {
     lazyloadImages = document.querySelectorAll(".lazy");
-    var imageObserver = new IntersectionObserver(function(entries, observer) {
-      entries.forEach(function(entry) {
+    var imageObserver = new IntersectionObserver(function (entries, observer) {
+      entries.forEach(function (entry) {
         if (entry.isIntersecting) {
           var image = entry.target;
           image.classList.remove("lazy");
@@ -81,27 +81,27 @@ for (var i = 0; i < label.length; i++) {
       });
     });
 
-    lazyloadImages.forEach(function(image) {
+    lazyloadImages.forEach(function (image) {
       imageObserver.observe(image);
     });
-  } else {  
+  } else {
     var lazyloadThrottleTimeout;
     lazyloadImages = document.querySelectorAll(".lazy");
-    
-    function lazyload () {
-      if(lazyloadThrottleTimeout) {
-        clearTimeout(lazyloadThrottleTimeout);
-      }    
 
-      lazyloadThrottleTimeout = setTimeout(function() {
+    function lazyload() {
+      if (lazyloadThrottleTimeout) {
+        clearTimeout(lazyloadThrottleTimeout);
+      }
+
+      lazyloadThrottleTimeout = setTimeout(function () {
         var scrollTop = window.pageYOffset;
-        lazyloadImages.forEach(function(img) {
-            if(img.offsetTop < (window.innerHeight + scrollTop)) {
-              img.src = img.dataset.src;
-              img.classList.remove('lazy');
-            }
+        lazyloadImages.forEach(function (img) {
+          if (img.offsetTop < (window.innerHeight + scrollTop)) {
+            img.src = img.dataset.src;
+            img.classList.remove('lazy');
+          }
         });
-        if(lazyloadImages.length == 0) { 
+        if (lazyloadImages.length == 0) {
           document.removeEventListener("scroll", lazyload);
           window.removeEventListener("resize", lazyload);
           window.removeEventListener("orientationChange", lazyload);
@@ -117,27 +117,60 @@ for (var i = 0; i < label.length; i++) {
 
 
 
- 
 
- $(document).ready(function(){
-			$('.customer-logos').slick({
-				slidesToShow: 5,
-				slidesToScroll: 1,
-				autoplay: true,
-				autoplaySpeed: 1000,
-				arrows: false,
-				dots: false,
-					pauseOnHover: false,
-					responsive: [{
-					breakpoint: 768,
-					settings: {
-						slidesToShow: 3
-					}
-				}, {
-					breakpoint: 520,
-					settings: {
-						slidesToShow: 2
-					}
-				}]
-			});
-		});
+
+const sidebar = document.querySelector('.pop-up-form');
+const overlay = document.querySelector('.pop-up-overlay');
+const bars = document.querySelector('.add');
+
+
+
+bars.addEventListener('click', function () {
+  sidebar.classList.toggle('show');
+  overlay.classList.add('show')
+});
+overlay.addEventListener('click', function () {
+  sidebar.classList.remove('show');
+  sidebar.style.transition = '0.3s ease-in'
+  overlay.classList.remove('show')
+})
+
+
+$(document).ready(function () {
+  $('.customer-logos').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 3
+      }
+    }, {
+      breakpoint: 520,
+      settings: {
+        slidesToShow: 2
+      }
+    }]
+  });
+});
+
+
+
+
+const Karachi = ['karachi-1', 'karachi-2', 'karachi-3', 'karachi-4', 'karachi-5'];
+
+const city = document.getElementById('city');
+
+
+Karachi.forEach(e => {
+
+  city.append = e;
+  log(e)
+})
+
+console.log(city.options.innerHTML);
